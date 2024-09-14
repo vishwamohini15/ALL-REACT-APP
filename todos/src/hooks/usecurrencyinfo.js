d4500a2cba0a64440f2a185a18a5f7bab8c35c4f
vishwamohini15 <vishwamohini0015@gmail.com>
2024-09-14 20:39:10 +0530

    first commit

---
+++ todos/src/hooks/usecurrencyinfo.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+function usecurrencyinfo(currency){
+     const [data, setdata]=useState({})
+     useEffect(() => {
+      fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+      .then((res)=>res.json())
+      .then((res)=>setdata(res[currency]))
+      console.log(data);
+     }, [currency])
+     console.log(data);
+     return data
+}
+export default usecurrencyinfo
